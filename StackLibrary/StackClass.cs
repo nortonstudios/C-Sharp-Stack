7d69e9051e88728223304e54296d2469421a6266
Mike <mike@Mikes-MacBook-Pro.local>
2021-03-10 22:33:12 -0700

    First commit

---
+++ StackLibrary/StackClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//This class holds the control methods for my stack data structure implementation.
+namespace StackLibrary
+{
+    public class StackClass
+    {
+
+        public StackNode Head { get; set; }
+
+        public static void Push( StackNode node)
+        {
+
+
+        }
+
+        public static object Pop ()  ////START HERE
+        {
+            object payload = Head.Payload;
+            Head = Head.NextNode;
+            return payload;
+        }
+
+
+        public StackClass()
+        {
+        }
+    }
+}
---
+++ StackLibrary/StackNode.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StackLibrary
+{
+    public class StackNode
+    {
+        public StackNode()
+        {
+        }
+    }
+}
