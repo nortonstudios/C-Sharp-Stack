1839bbdf2231a760c15145c4d094e8725a3a9f91
Mike <mike@Mikes-MacBook-Pro.local>
2021-03-14 13:57:21 -0600

    Added a palindrome finder tool, tests, and a console UI.

--- StackLibrary.Tests/PaliToolsTests.cs
+++ StackLibrary.Tests/PaliToolsTests.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using Xunit;
+
+
+
 namespace StackLibrary.Tests
 {
     public class PaliToolsTests
     {
-        public PaliToolsTests()
+        [Theory]
+        [InlineData("Spacecar", false)]
+        public void SouldReturnFalse(string input, bool expected)
         {
+            //Arange
+            bool actual;
+            PaliTools Palindrome = new PaliTools();
+
+            //Act
+            actual = Palindrome.IsPalindrome(input);
+
+            //Assert
+            Assert.Equal(actual, expected);
         }
+
+        [Theory]
+        [InlineData("Racecar", true)]
+        public void ShouldReturnTrue(string input, bool expected)
+        {
+            //Arragne
+            bool actual;
+            PaliTools Palindrome = new PaliTools();
+
+            //Act
+            actual = Palindrome.IsPalindrome(input);
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("", true)]
+        public void WhatDoesAnEmptyStringDo(string input, bool expected)
+        {
+            //Arragne
+            bool actual;
+            PaliTools Palindrome = new PaliTools();
+
+            //Act
+            actual = Palindrome.IsPalindrome(input);
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
     }
 }
--- StackLibrary/PaliTools.cs
+++ StackLibrary/PaliTools.cs
@@ -3,6 +3,52 @@ namespace StackLibrary
 {
     public class PaliTools
     {
+        //Fields and props
+       // public string TestString { get; set; }
+
+
+        //Methods
+
+        //Default test method is not case sensitive.
+        public bool IsPalindrome(string testString)
+        {
+            return PalandromeTester(testString.ToLower());
+        }
+
+        //Case sensitive option.
+        public bool IsPalindromeCaseSensitive(string testString)
+        {
+            return PalandromeTester(testString);
+        }
+
+        //Business logic of palindrome test class.
+        //Called by visible methods to do the dirty work. 
+        private bool PalandromeTester(string testString)
+        { 
+            int halfString = (int)(testString.Length / 2);
+            StackClass Stack = new StackClass();
+
+            for (int i = 0; i < halfString; i++)
+            {
+                Stack.Push(testString[i]);
+            }
+
+            string backHalf = testString.Substring(testString.Length - halfString, halfString);
+
+            for (int i = 0; i < backHalf.Length; i++)
+            {
+                if (backHalf[i].Equals(Stack.Pop()) != true)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;        
+        }
+
+
+        //Constructors
         public PaliTools()
         {
         }
